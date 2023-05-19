3e6dd5063871cd9054dd82ea4284b2d47c563445
IgorGuariroba <1g0r.guari@gmail.com>
2023-05-19 19:33:33 -0300

    Build prod

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portgit',
+  base: '/portgit/',
   build: {
     outDir: 'docs',
-    assetsDir: 'assets',
-    sourcemap: false,
+    assetsDir: '.',
+    sourcemap: true,
     emptyOutDir: true,
   },
 })
